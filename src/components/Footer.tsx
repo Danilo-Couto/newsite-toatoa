c333dd6bcab1c627b362c6fcdf7def7ae02393f8
Danilo Couto <danilocoutodev@gmail.com>
2025-11-05 12:54:04 -0300

    add hbook

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,20 @@
+import { useEffect } from "react";
+
 export default function Footer() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://s3-sa-east-1.amazonaws.com/hbook-universal-js/js/632b13f5e819f634133f8f8a.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  // const handleWhatsAppClick = () => {
+  //   const message = encodeURIComponent("Olá, vim pelo site.");
+  //   window.open(`https://wa.me/558481096734?text=${message}`, "_blank");
+  // };
 
   return (
     <footer>
-      {<div className="floating-whatsapp-button"
-            onClick={() => {
-              window.open('https://wa.me/558481096734?text="olá, vim pelo site."', '_blank');
-            } }>
-        <img loading='lazy' decoding="async" src="/WhatsAp.webp" alt="WhatsApp Logo" />
-      </div>  }
     </footer>
-    );
-  }
-  
+  );
+}
