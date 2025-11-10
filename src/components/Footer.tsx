b6b0ada973a517d954681c0a004f1be80a6e8c72
Danilo Couto <danilocoutodev@gmail.com>
2025-11-10 10:35:43 -0300

    style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,10 +11,17 @@ export default function Footer() {
   return (
     <footer>
       <div className="floating-whatsapp-button"
-          onClick={() => {
-            window.open('https://wa.me/558481096734?text="olá, vim pelo site."', '_blank');
-          } }>
-          <img loading='lazy' decoding="async" src="/WhatsAp.webp" alt="WhatsApp Logo" />
+        onClick={() => {
+          window.open('https://wa.me/558481096734?text="olá, vim pelo site."', '_blank');
+        } }>
+        <img loading='lazy' decoding="async" src="/WhatsAp.webp" alt="WhatsApp Logo" />
+      </div>
+       <div>
+          <a
+          href='https://hbook.hsystem.com.br/Booking?companyId=632b13f5e819f634133f8f8a&promocode=igdHnaPz'
+          target="_blank"
+          rel="noopener noreferrer"
+          className="room-button" />
       </div>  
     </footer>
   );
